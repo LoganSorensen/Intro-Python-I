2ac7a9a77b5e9f831356515e34793a2d21aadbcd
Logan Sorensen <loganmsorensen@gmail.com>
2020-05-05 15:38:58 -0700

    completed directories 11-15

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,10 +10,16 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
-
+file1 = open("src/foo.txt", "r")
+print(file1.read())
+file1.close()
+# print(file1.read())  # checks if file has been closed
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+file2 = open("src/bar.txt", "a")
+file2.write("Line 1\nLine 2\nLine 3")
+file2.close()
